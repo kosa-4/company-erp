0ba1cffd4d5ac5d5c7e52e92271c5ab9679cdd47
jaemin26 <ljaemjn26@gmail.com>
2025-12-30 15:23:34 +0900

    Chore: Backend 프로젝트 초기 설정 Resolves: #4

---
+++ src/main/java/com/company/erp/ErpProjectApplication.java
@@ -0,0 +1,14 @@
+package com.company.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpProjectApplication.class, args);
+		System.out.println("Server started successfully");
+	}
+
+}
