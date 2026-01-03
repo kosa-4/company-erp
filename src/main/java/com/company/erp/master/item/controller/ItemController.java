8c4c757f7bb07db984efa7e67cd642917c925687
jin <chlwlsgus543@gmail.com>
2026-01-03 10:03:15 +0900

    controller cors setting

--- src/main/java/com/company/erp/master/item/controller/ItemController.java
+++ src/main/java/com/company/erp/master/item/controller/ItemController.java
@@ -5,12 +5,14 @@ import com.company.erp.master.item.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/master/item")
 public class ItemController {
@@ -24,6 +26,7 @@ public class ItemController {
             List<ItemListDto> items = itemService.getItemList();
             return ResponseEntity.ok(items);
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
