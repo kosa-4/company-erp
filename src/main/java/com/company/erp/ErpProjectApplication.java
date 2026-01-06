3a5e010f276233396d5bcdcbe3f797829fa85256
minseon48 <97889824+minseon48@users.noreply.github.com>
2026-01-06 11:24:31 +0900

    Revert "[Feat] 채번 로직 생성 (#42)"
    This reverts commit 51700f5e54070d180d62b3a4994e9365f3d979b7.

--- src/main/java/com/company/erp/ErpProjectApplication.java
+++ src/main/java/com/company/erp/ErpProjectApplication.java
@@ -1,10 +1,8 @@
 package com.company.erp;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.company.erp")
 @SpringBootApplication
 public class ErpProjectApplication {
 
--- src/main/java/com/company/erp/common/docNum/service/ResetUnit.java
+++
@@ -1,7 +0,0 @@
-package com.company.erp.common.docNum.service;
-
-public enum ResetUnit {
-    DAILY,
-    YEARLY,
-    NONE
-}
