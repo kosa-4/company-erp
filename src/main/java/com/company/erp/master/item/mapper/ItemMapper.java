7192deb6c848c61fa28b76d559f7e09f83e679ba
JinHyunChoiJinHyun <151479587+JinHyunChoiJinHyun@users.noreply.github.com>
2026-01-07 11:18:37 +0900

    [Feat] 품목 조회 및 등록 기능 추가 (#53)
    * 백엔드 로직 추가
    
    * select front edit
    
    * remove application.properties
    
    * change front
    
    * remove properties
    
    * controller cors setting
    
    * complete select
    
    * modify item page
    
    * add page and filter
    
    * modify item mapper
    
    * modify service
    
    * modify item page
    
    * add 주석 item page
    
    * add search filter
    
    * delete print
    
    * search param mapping itemSearchDto
    
    * pull develop
    
    * add get detail item
    
    * add detail item
    
    * add detail item
    
    * add register item
    
    * add detail
    
    * modify mapper
    
    * add save item
    
    * count doc num
    
    * modify service add enum
    
    * Feat : 등록 기능 수정
    
    * Feat : 등록 기능 수정
    
    * feat: mapper parameter 수정
    
    * feat: 품목 등록
    
    * 품목 등록 프론트 수정
    
    ---------
    
    Co-authored-by: minseon48 <97889824+minseon48@users.noreply.github.com>
    Co-authored-by: jaemin26 <ljaemjn26@gmail.com>

---
+++ src/main/java/com/company/erp/master/item/mapper/ItemMapper.java
@@ -0,0 +1,28 @@
+package com.company.erp.master.item.mapper;
+
+import com.company.erp.master.item.dto.ItemDetailDto;
+import com.company.erp.master.item.dto.ItemDto;
+
+import com.company.erp.master.item.dto.ItemSearchDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+//@Mapper // MERGE 시 삭제
+public interface ItemMapper {
+    /* 조회 */
+    // item 조회
+    List<ItemDetailDto> selectItemList(ItemSearchDto searchDto);
+    // item 수 계산
+    int countItemList(ItemSearchDto searchDto);
+    // 중복 여부 체크
+    boolean existsByNameAndSpec(ItemDetailDto itemDetailDto);
+    // 상세 품목 조회
+    ItemDetailDto selectItemByCode(String code);
+
+    /* 등록 */
+    // 품목 마스터 등록
+    void insertItemMTGL(ItemDetailDto itemDetailDto);
+    // 품목 카테고리 등록
+    void insertItemMTGC(ItemDetailDto itemDetailDto);
+}
---
+++ src/main/java/com/company/erp/master/item/service/ItemService.java
@@ -0,0 +1,62 @@
+package com.company.erp.master.item.service;
+
+import com.company.erp.common.docNum.dto.DocNumDTO;
+import com.company.erp.common.docNum.service.DocKey;
+import com.company.erp.common.docNum.service.DocNumService;
+import com.company.erp.master.item.dto.*;
+import com.company.erp.master.item.mapper.ItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ItemService {
+    @Autowired
+    ItemMapper itemMapper;
+    @Autowired
+    DocNumService docNumService;
+
+    /* 조회 */
+    public ItemResponseDto<ItemDetailDto> getItemList(ItemSearchDto searchDto) {
+        // 1. 총 품목 수 계산
+        int totalCount = itemMapper.countItemList(searchDto);
+        // 2. 총 페이지 계산
+        int totalPage = (int)Math.ceil((double) totalCount / searchDto.getPageSize());
+        // 3. Dto 반환
+        return new ItemResponseDto<ItemDetailDto>(
+                itemMapper.selectItemList(searchDto),
+                searchDto.getPage(),
+                searchDto.getPageSize(),
+                totalPage,
+                totalCount
+        );
+    }
+
+    public ItemDetailDto getItemDetail(String code) {
+        return itemMapper.selectItemByCode(code);
+    }
+
+    /* 등록 */
+    @Transactional
+    public void registerItem(ItemDetailDto itemDetailDto) {
+        // 1. 중복 체크
+        boolean existsItem = itemMapper.existsByNameAndSpec(itemDetailDto);
+        if(existsItem){
+            throw new RuntimeException("동일한 이름과 규격의 품목이 존재합니다.");
+        }
+
+        // 2. 중복 아닐 시
+
+        // 2-1. 체번 계산
+        String itemCode = docNumService.generateDocNumStr(DocKey.IT);
+        itemDetailDto.setItemCode(itemCode);
+        // 2-2. enum 입력
+        itemDetailDto.setStatus(ItemStatus.REGISTERED.getValue());
+        itemDetailDto.setUseYn(ItemUse.YES.getValue());
+        itemDetailDto.setDeleteYn(ItemDelete.NO.getValue());
+        // 2-3. 품목 마스터 등록
+        itemMapper.insertItemMTGL(itemDetailDto);
+        // 2-4. 품목 카테고리 등록
+        itemMapper.insertItemMTGC(itemDetailDto);
+    }
+}
