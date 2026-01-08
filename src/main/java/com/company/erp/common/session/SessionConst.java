468efa265d7c89f404bb15b702a2efb4d73408b1
soyeon <tmzkffos@gmail.com>
2026-01-08 16:34:07 +0900

    feat: add session module

---
+++ src/main/java/com/company/erp/common/session/SessionConst.java
@@ -0,0 +1,8 @@
+package com.company.erp.common.session;
+
+public final class SessionConst {
+    private SessionConst() {}
+
+    public static final int STATUS_NO_SESSION = 440;
+    public static final int STATUS_MULTI_LOGIN = 441;
+}
