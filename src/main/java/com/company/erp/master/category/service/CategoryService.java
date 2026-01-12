b9632bd6beb3c533d69cffed5bc91af16d045d07
JinHyunChoiJinHyun <151479587+JinHyunChoiJinHyun@users.noreply.github.com>
2026-01-12 15:52:23 +0900

    [Feat] 카테고리 페이지 구현 (#81)
    요구사항
    1. 카테고리 생성 및 조회 페이지 구현
    2. 품목 등록 메뉴 품목 종류 클릭 시 카테고리 선택 가능
    
    설명
    자잘한 오류는 무시하고 우선 merge 하겠습니다!

---
+++ src/main/java/com/company/erp/master/category/service/CategoryService.java
@@ -0,0 +1,76 @@
+package com.company.erp.master.category.service;
+
+import com.company.erp.master.category.dto.CategoryDto;
+import com.company.erp.master.category.dto.CategoryListDto;
+import com.company.erp.master.category.mapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryMapper categoryMapper;
+
+    /* 조회 */
+    // 전체 카테고리 조회
+    public List<CategoryListDto> getAllCategories(){
+        return categoryMapper.selectAllCategories();
+    }
+    // 선택된 카테고리 목록 조회
+    public List<CategoryListDto> getCategoryList(CategoryListDto  categoryListDto) {
+        return categoryMapper.selectCategoryList(categoryListDto);
+    }
+
+    // 단일 카테고리 조회
+    public CategoryListDto getCategoryByCode(String itemCls){
+        return categoryMapper.selectCategoryByCode(itemCls);
+    }
+
+    /* 저장 */
+    @Transactional
+    public void registerCategory(List<CategoryListDto> categoryListDto){
+
+        // 1. 데이터 가공 (프론트에서도 가능하나 조작 위험 방지를 위해 서비스에서 가공)
+        List<CategoryListDto> filteredList = new ArrayList<>();
+
+        for(CategoryListDto dto: categoryListDto){
+            // 1-1. 분류 체크
+            switch (dto.getItemLvl()){
+                case 0:
+                    filteredList.add(dto);
+                    break;
+                case 1, 2, 3:
+                    String parentCls = dto.getParentItemCls();
+                    String maxChildCls = categoryMapper.selectMaxChild(parentCls);
+                    if(maxChildCls != null){
+                        int itemCls =  Integer.parseInt(maxChildCls.substring(parentCls.length()));
+                        dto.setItemCls(parentCls.concat(String.valueOf(itemCls + 1)));
+                    } else {
+                        dto.setItemCls(parentCls.concat("1"));
+                    }
+
+                    filteredList.add(dto);
+                    break;
+
+            }
+        }
+        // 2. 가공된 데이터가 존재하지 않을 시 예외 처리
+        if(filteredList.isEmpty()){
+            throw new RuntimeException("저장할 데이터가 없습니다.");
+        }
+        // 3. 중복 체크
+        int existsCategory = categoryMapper.existsCategory(filteredList);
+
+        if(existsCategory > 0){
+            throw new RuntimeException("동일한 카테고리가 존재합니다.");
+        }
+
+        // 4. 최종 저장
+        categoryMapper.insertCategory(filteredList);
+    }
+
+}
