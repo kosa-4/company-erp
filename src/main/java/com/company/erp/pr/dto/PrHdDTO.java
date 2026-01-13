8aa7bdc535977635c57a8585579c7e8b1d8b99eb
MINSEON\minseon <duiph558291@gmail.com>
2026-01-13 17:37:26 +0900

    Feat : PRHD 테이블 데이터 저장용 DTO 생성

---
+++ src/main/java/com/company/erp/pr/dto/PrHdDTO.java
@@ -0,0 +1,28 @@
+package com.company.erp.pr.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+//저장용 hd DTO
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PrHdDTO {
+    private String prNum;//구매요청번호
+    private String regUserId;//등록자id
+    private String delFlag;//삭제여부
+    private String prSubject;//구매요청명
+    private String deptCd;//구매요청부서
+    private BigDecimal prAmt;//구매요청 총 금액
+    private String reqUserId;//구매요청자id
+    private String progressCd;//상태
+    private String rmk;//비고
+    private String pcType;//구매유형
+
+}
