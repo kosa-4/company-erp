63b8a5fd7d3f302128150c0851c1d45d6584dd1e
MINSEON\minseon <duiph558291@gmail.com>
2026-01-15 19:15:40 +0900

    Rename : PrListResponse DTO 필드명 변경

--- src/main/java/com/company/erp/pr/dto/PrListResponse.java
+++ src/main/java/com/company/erp/pr/dto/PrListResponse.java
@@ -12,8 +12,8 @@ public class PrListResponse {
     private String progressCd;//상태
     private String prNum;//pr번호
     private String pcType;//구매유형
-    private String reqUserId;//요청자(userNm과 동일로 우선 판단)
-    private String deptNm;//부서명
+    private String requester;//요청자(userNm과 동일로 우선 판단)
+    private String deptName;//부서명
     private Date regDate;//요청일(regDate 등록일과 동일)
 
     //prdt에서 가져올 데이터
