a7543174f8ff06972fdb2f8fa56eaeb5767b1934
MINSEON\minseon <duiph558291@gmail.com>
2026-01-15 19:16:14 +0900

    Feat : PrMapper selectPrList에 param annotation 추가

--- src/main/java/com/company/erp/pr/mapper/PrMapper.java
+++ src/main/java/com/company/erp/pr/mapper/PrMapper.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface PrMapper {
+    String selectUserName(String userId);
+
     void insertPrHd(PrHdDTO prHdDTO);
     void insertPrDt(@Param("prDtList") List<PrDtDTO> prDtDTOList);
 
@@ -16,9 +18,13 @@ public interface PrMapper {
     void deletePrHd(@Param("prNum") String prNum);
     void deletePrDt(@Param("prNum") String prNum);
 
-    List<PrListResponse> selectPrList(String prNum,String prSubject,
-                                      String reqUserId,String deptCd,
-                                      String progressCd);
+    List<PrListResponse> selectPrList(@Param("prNum") String prNum,
+                                      @Param("prSubject") String prSubject,
+                                      @Param("requester") String requester,
+                                      @Param("deptName") String deptName,
+                                      @Param("progressCd") String progressCd,
+                                      @Param("startDate") String startDate,
+                                      @Param("endDate") String endDate);
 
 
 }
