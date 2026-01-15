96b09fa302000e94c1ddf2660657d1cdc4637370
MINSEON\minseon <duiph558291@gmail.com>
2026-01-15 18:02:10 +0900

    Feat : 초기 조회 및 구매요청 등록 시 세션 적용

--- src/main/java/com/company/erp/pr/controller/PrController.java
+++ src/main/java/com/company/erp/pr/controller/PrController.java
@@ -1,9 +1,11 @@
 package com.company.erp.pr.controller;
 
+import com.company.erp.common.session.SessionUser;
 import com.company.erp.pr.dto.PrItemDTO;
 import com.company.erp.pr.dto.PrListResponse;
 import com.company.erp.pr.dto.PrRequest;
 import com.company.erp.pr.service.PrService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +23,12 @@ public class PrController {
 
     //구매요청화면 초기 데이터 조회
     @GetMapping("/init")
-    public ResponseEntity<Map<String,Object>> initPurchase(){
-        Map<String, Object> initData = prService.initPurchaseData();
+    public ResponseEntity<Map<String,Object>> initPurchase(HttpSession httpSession){
+        SessionUser user = (SessionUser) httpSession.getAttribute(SessionUser.class.getName());
+        String userId = user.getUserId();
+        String deptName = user.getDeptName();
+
+        Map<String, Object> initData = prService.initPurchaseData(userId,deptName);
 
         return ResponseEntity.ok(initData);
     }
@@ -44,9 +50,14 @@ public class PrController {
 
     //구매요청 등록
     @PostMapping("/save")
-    public ResponseEntity<Map<String,String>> savePurchaseRequest(@RequestBody PrRequest prRequest){
+    public ResponseEntity<Map<String,String>> savePurchaseRequest(HttpSession httpSession,
+                                                                  @RequestBody PrRequest prRequest){
 
-        prService.insertPr(prRequest);
+        SessionUser user = (SessionUser) httpSession.getAttribute(SessionUser.class.getName());
+        String userId = user.getUserId();
+        String deptCd = user.getDeptCd();
+
+        prService.insertPr(userId,deptCd,prRequest);
 
         Map<String, String> response = new HashMap<>();
         response.put("message", "구매요청 등록 완료");
@@ -61,25 +72,29 @@ public class PrController {
         return ResponseEntity.ok(prItems);
     }
 
-    //구매요청 삭제
-    @PutMapping("/{prNum}/delete")
-    public ResponseEntity<String> deletePurchaseRequest(@PathVariable String prNum){
-        prService.deletePrRequest(prNum);
-
-        return ResponseEntity.ok().body("구매요청 삭제 완료");
-    }
-
 
     //구매요청현황 조회
-    @GetMapping("/list")                              //요청자,부서는 session 정보 통해 요청자 id값, 부서코드값으로 변경 예정
     public ResponseEntity<List<PrListResponse>> getPurchaseList(@RequestParam(required = false) String prNum,
                                                                 @RequestParam(required = false) String prSubject,
                                                                 @RequestParam(required = false) String requester,
                                                                 @RequestParam(required = false) String deptName,
-                                                                @RequestParam(required = false) String progressCd){
+                                                                @RequestParam(required = false) String progressCd,
+                                                                @RequestParam(required = false) String startDate,
+                                                                @RequestParam(required = false) String endDate){
 
-        List<PrListResponse> prList = prService.selectPrList(prNum,prSubject,requester,deptName,progressCd);
+        List<PrListResponse> prList = prService.selectPrList(prNum,prSubject,requester,deptName,progressCd,startDate,endDate);
 
         return ResponseEntity.ok(prList);
+    }
+
+    //구매요청 삭제
+    @PutMapping("/delete")
+    public ResponseEntity<Map<String,String>> deletePurchaseRequest(@RequestParam(required = true) String prNum,
+                                                                    @RequestParam(required = true) String itemCd){
+        prService.deletePrRequest(prNum,itemCd);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "구매요청 삭제완료");
+        return ResponseEntity.ok().body(response);
     }
 }
