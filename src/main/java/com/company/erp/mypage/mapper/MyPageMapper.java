d6c61ad286ae6ffaf20d7d582256088b2cd23fcf
MINSEON\minseon <duiph558291@gmail.com>
2026-01-17 20:12:25 +0900

    feat: Mapper에 협력사 조회 메서드 추가

--- src/main/java/com/company/erp/mypage/mapper/MyPageMapper.java
+++ src/main/java/com/company/erp/mypage/mapper/MyPageMapper.java
@@ -4,6 +4,8 @@ import com.company.erp.mypage.dto.MyInfoDTO;
 import com.company.erp.mypage.dto.MyInfoUpdateDTO;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Map;
+
 public interface MyPageMapper {
 
     MyInfoDTO selectMyInfo(
@@ -16,4 +18,8 @@ public interface MyPageMapper {
     );
     
     int updateMyInfo(MyInfoUpdateDTO dto);
+
+    Map<String, String> selectVendorUserInfo(@Param("userId") String userId);
+
+    int updateVendorUserInfo(@Param("userId") String userId, @Param("password") String password);
 }
